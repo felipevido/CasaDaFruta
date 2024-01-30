2d741c1e243d697be85207c7c21d1190d8e3d047
Felipe Vido <contatofelipevido@gmail.com>
2024-01-30 19:26:49 -0300

    Update Index

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var meuBotao = document.getElementById("meuBotao");
+    meuBotao.addEventListener("click", function () {
+        alert("\u{2699} Progresso do Site\n\n\u{2705} Identidade Visual Criada\n\u{2705} Hospedagem Contratada\n\u{2705} Provedor Contratado\n\u{2705} Página Sobre Nós\n\u{2705} Página Contato\n\u{2705} Página FAQ\n\u{2705} Guia de Uso ao Usuário\n\u{2705} Política de Privacidade\n\u{2705} Termos de Uso\n\u{2705} Blog (Receitas e Dicas)\n\n\u{1F550} Construíndo Vitrine de Produtos");
+    });
+});
